60e130f604b7b9099c66ebee9bacddee27909b22
Kevinpizza123 <kap.sani@yavirac.edu.ec>
2023-02-07 17:57:02 -0500

    Sistema de Ventas GameShop

--- servernest/src/videogames/dto/update-videogame.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateVideogameDto } from './create-videogame.dto';
-
-export class UpdateVideogameDto extends PartialType(CreateVideogameDto) {}
